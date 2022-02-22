af0e1c610cc3702fdcae4362e4dd2d26871a8b38
samsonsanusi <samsonsanusi96@gmail.com>
2022-02-22 14:14:46 +0100

    completed forgot password screen

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Login from "./login";
+import ForgotPassword from "./forgotPassword";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
         <div className="App">
             <Routes>
                 <Route path="/" element={<Login />} />
+                <Route path="/forgotPassword" element={<ForgotPassword />} />
             </Routes>
         </div>
     );
